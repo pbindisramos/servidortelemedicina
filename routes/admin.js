5298762f63b91c1e55c425097f40426f3afbbe41
pbindisramos <p.bindisramos@gmail.com>
2021-03-08 08:16:31 -0300

    admin crear agenda

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const doctorController = require('../controllers/doctorController');
+const AgendaController = require('../controllers/agendaController');
 const { check } = require('express-validator');
 const auth = require('../middleware/auth');
 //const verificarRoles = require('../middleware/verificarRoles');
@@ -28,5 +29,13 @@ router.get('/', auth.jwt, auth.isAdmin, doctorController.getMedicos);
 router.put('/:id', auth.jwt, auth.isAdmin, doctorController.actualizarMedico);
 //eliminar medico
 router.delete('/:id', auth.jwt, auth.isAdmin, doctorController.eliminarMedico);
+
+//agendar horas
+router.post(
+  '/horasmedicas',
+  auth.jwt,
+  auth.isAdmin,
+  AgendaController.crearAgenda
+);
 
 module.exports = router;
