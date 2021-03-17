0f3e883b59faec9a93bc7777656179f97f2564d7
pbindisramos <p.bindisramos@gmail.com>
2021-03-17 10:58:11 -0300

    agenda

--- controllers/agendaController.js
+++ controllers/agendaController.js
@@ -2,12 +2,19 @@ const Agenda = require('../models/Agenda');
 
 exports.crearAgenda = async (req, res, next) => {
   try {
-    const agenda = new Agenda(req.body);
-    agenda.doctor = req.usuario.id;
-    agenda.save();
-    res.json(agenda);
+    console.log(req.body.days.Lunes[0].manana_comienzo);
   } catch (error) {
     console.log(error);
-    res.status(500).send('Hubo un error');
   }
+  // try {
+
+  //   //console.log(req.body);
+  //   const agenda = new Agenda(req.body);
+  //   agenda.doctor = req.usuario.id;
+  //   agenda.save();
+  //   res.json(agenda);
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).send('Hubo un error');
+  // }
 };
